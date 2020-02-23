fba5002e1f0c5b26fe6a98d6a7afc7b6678ad8f2
Terrell Ibanez <9453223+ibanezt@users.noreply.github.com>
2020-02-23 12:30:08 -0500

    Integrate Scraper into API

--- Backend/API_Functions/functionScraper.py
+++ Backend/API_Functions/functionScraper.py
@@ -9,13 +9,15 @@ import re
 from urllib.parse import urlparse
 import urllib
 # Parses the data from the page
+
+
 def grabData(self, windowAddress):
     link = urlparse(windowAddress).path
     filteredoutlink = re.findall(r'(dp/\S+)', link)
-    # initialize an empty string 
-    str1 = " " 
-    
-    # return string   
+    # initialize an empty string
+    str1 = " "
+
+    # return string
     newLink = str1.join(filteredoutlink)
     newLink = newLink.replace("dp/", "")
     reviews = []
@@ -32,21 +34,21 @@ def grabData(self, windowAddress):
         break
 
     product_json['catagories'] = []
-    for span in soup.findAll('span', class_ = 'nav-a-content'):
+    for span in soup.findAll('span', class_='nav-a-content'):
         if span.text:
             catagories = span.text.strip()
             product_json['catagories'] = catagories
             break
-        
+
     # This block of code will help extract the average star rating of the product
     for i_tags in soup.findAll('i',
-                            attrs={'data-hook': 'average-star-rating'}):
+                               attrs={'data-hook': 'average-star-rating'}):
         for spans in i_tags.findAll('span', attrs={'class': 'a-icon-alt'}):
             product_json['star-rating'] = spans.text.strip()
             break
     # This block of code will help extract the number of customer reviews of the product
     for spans in soup.findAll('span', attrs={'id': 'acrCustomerReviewText'
-                            }):
+                                             }):
         if spans.text:
             review_count = spans.text.strip()
             product_json['customer-reviews-count'] = review_count
@@ -73,25 +75,26 @@ def grabData(self, windowAddress):
         # This block of code will help extract the long reviews of the product
     product_json['long-reviews'] = []
     for divs in soup.findAll('div', attrs={'data-hook': 'review-collapsed'
-                                }):
+                                           }):
         long_review = divs.text.strip()
         product_json['long-reviews'].append(long_review)
-        
+
     product_json['asin'] = []
     product_json['asin'].append(asin)
-    print("Name " + str(product_json["name"]) + "\nAsin: " + str(product_json["asin"]) + "\nLong reviews: " + str(product_json["long-reviews"]) + "\n")
-    
+    print("Name " + str(product_json["name"]) + "\nAsin: " + str(
+        product_json["asin"]) + "\nLong reviews: " + str(product_json["long-reviews"]) + "\n")
 
-# Grabs the animal text from user input and number of pics we'll use
-windowAddress = "https://www.amazon.com/EVIL-ENERGY-Exhaust-Muffler-Universal/dp/B01N6EIB3B/ref=sr_1_3?keywords=muffler&qid=1582468234&sr=8-3"
-product_json = {}
-        
-# Grabs the current URL
-result = requests.get(windowAddress)
-src = result.content
-# Instantate beautifulsoup object
-data = BeautifulSoup(src, 'lxml')
-grabData("", windowAddress)
 
-    
-        
+def scrape(request):
+    request_json = request.get_json()
+    windowAddress = request_json['address']
+
+    # Grabs the animal text from user input and number of pics we'll use
+    product_json = {}
+
+    # Grabs the current URL
+    result = requests.get(windowAddress)
+    src = result.content
+    # Instantate beautifulsoup object
+    data = BeautifulSoup(src, 'lxml')
+    grabData("", windowAddress)
