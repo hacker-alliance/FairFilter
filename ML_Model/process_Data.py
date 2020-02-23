3b5fdfc3712244366b7da1fb55fd719a31ae075a
samanthadaugherty <sedaugherty6@gmail.com>
2020-02-22 21:24:57 -0500

    added code to make sample

--- ML_Model/process_Data.py
+++ ML_Model/process_Data.py
@@ -19,13 +19,28 @@ def getDF(path):  # Get Dataframe After Parsing
 
 
 dfs = []
+dfs_sample = []
 
 for filename in os.listdir('./datasets'):
     if (filename != 'Datasets.md'):
         fdf = getDF('./datasets/' + filename)
         needed = pd.DataFrame({'review': (fdf['summary'] + ". " + fdf['reviewText']).replace(',', ' '), 'category': filename[8:-10]})
+        # sample100 = needed.sample(n = 100)
         dfs.append(needed)
+        # dfs_sample.append(sample100)
 
 df = pd.concat(dfs)
-
 df.to_csv('out.csv', header=False, index=False)
+
+# comment out ines 21-31 if you ONLY need to grab the sample.
+# if you need to grab both, comment the for loop out and uncomment the commented
+# lines in the above for loop
+for filename in os.listdir('./datasets'):
+    if (filename != 'Datasets.md'):
+        fdf = getDF('./datasets/' + filename)
+        needed = pd.DataFrame({'review': (fdf['summary'] + ". " + fdf['reviewText']).replace(',', ' '), 'category': filename[8:-10]})
+        sample100 = needed.sample(n = 100)
+        dfs_sample.append(sample100)
+
+df_sample = pd.concat(dfs_sample)
+df_sample.to_csv('out_sample.csv', header=False, index=False)
